7d1c092380a46ce81c085dd77d70c62eb53da43e
Mari Myakisheva <smi.tomsk@gmail.com>
2018-05-27 17:51:26 +0700

    last vers 27.50

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,15 +2,30 @@ $(document).ready(function() {
 
 $('.slider__list').slick();
 
+$('#fullpage').fullpage({
+  navigation: true
 });
 
-
-  $('.menu__elem').on('click', function () {
-    $('.menu__text').removeClass('menu__text__active');
-    $(this).addClass('menu__text__active');
+$('.acco__elem').on('click', function (e) {
+	e.preventDefault()
+  $('.acco__elem').removeClass('acco__elem__active');
+  $(this).addClass('acco__elem__active');
 });
 
-$('.menu__elem').on('click', function () {
-  $('.menu__text').removeClass('menu__text__active');
-  $(this).addClass('menu__text__active');
+$('.menu__elem').on('click', function (e) {
+	e.preventDefault()
+  $('.menu__elem').removeClass('menu__elem__active');
+  $(this).addClass('menu__elem__active');
+});
+
+$('.btn_reviews').on('click', function (e) {
+	e.preventDefault()
+  $('.reviews-popups').addClass('reviews-popups__active');
+});
+
+$('.reviews-popups__close').on('click', function (e) {
+	e.preventDefault()
+   $('.reviews-popups').removeClass('reviews-popups__active');
+});
+
 });
